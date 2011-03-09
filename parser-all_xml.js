4e880821f899d41fb7f16732f84f92de5f8fc3a8
justin <justin.ethier@gmail.com>
2011-03-09 13:27:58 -0500

    Used sync version of read file.

--- parser-all_xml.js
+++ parser-all_xml.js
@@ -16,22 +16,16 @@ jQuery = require('jquery');
 var files = fs.readdirSync('.');
 jQuery.each(files, function(index, filename){
     if (filename.substr(filename.length-4, 4) == ".xml") {
-        fs.readFile(filename, 'utf-8', function (err, data) {
-            if (err){
-                console.log("Error processing " + filename);
-                throw err;
-            }
+        data = fs.readFileSync(filename, 'utf-8');
+        var dataRow = {
+            station : jQuery("station_id", data).text(), 
+            location : jQuery("location", data).text(), 
+            suggested_pickup : jQuery("suggested_pickup", data).text(), 
+            suggested_pickup_period : jQuery("suggested_pickup_period", data).text(), 
+            latitude : jQuery("latitude", data).text(),
+            longitude : jQuery("longitude", data).text()
+        };
         
-            var dataRow = {
-                station : jQuery("station_id", data).text(), 
-                location : jQuery("location", data).text(), 
-                suggested_pickup : jQuery("suggested_pickup", data).text(), 
-                suggested_pickup_period : jQuery("suggested_pickup_period", data).text(), 
-                latitude : jQuery("latitude", data).text(),
-                longitude : jQuery("longitude", data).text()
-            };
-        
-            console.log(JSON.stringify(dataRow));
-        });
+        console.log(JSON.stringify(dataRow));
     }
 });
