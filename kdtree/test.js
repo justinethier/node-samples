75a96877b4c6ee2c7f040f63612e380e397e5ef0
justin <justin.ethier@gmail.com>
2011-03-13 01:35:04 -0500

    Adding tests for insert, nearest

--- kdtree/test.js
+++ kdtree/test.js
@@ -1,3 +1,8 @@
 var kd = require('kdtree');
 var tree = new kd.KDTree();
 tree.test();
+tree.insert(1, 2, 3);
+tree.insert(1, 2, 3);
+tree.insert(1, 2, 3);
+tree.insert(10, 20, 30);
+//tree.nearest(9, 19, 31);
